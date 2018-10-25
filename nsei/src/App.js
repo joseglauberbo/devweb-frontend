73aa8859020c209ecb17aef7b264693b62d77053
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2018-10-25 00:11:15 -0300

    finalizando tutorial 1

--- nsei/src/App.js
+++ nsei/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Code from './Code';
 
 class App extends Component {
   render() {
@@ -9,16 +10,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            DEVERIA ESTAR APARENCENDO ALGO JA, YUKEEE
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
