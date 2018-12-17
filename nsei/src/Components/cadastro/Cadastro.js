f8fc3403597d3a949057c535c78626e9eab57e56
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-12-17 01:03:21 -0300

    autenticacao quase la, uma hr eu consigo

--- nsei/src/Components/cadastro/Cadastro.js
+++ nsei/src/Components/cadastro/Cadastro.js
@@ -1,40 +1,117 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Form, Grid, Header, Confirm, Segment } from 'semantic-ui-react'
 import { NavLink } from "react-router-dom";
+import axios from 'axios'
+import { withRouter } from 'react-router';
 
-const Cadastro = () => (
-  <div className='login-form'>
-    <style>{`
+
+class Cadastro extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      realizado: false,
+      erro: false,
+      mensagemErro: "",
+    }
+  }
+
+  usernameChange = event => {
+    this.setState({ username: event.target.value });
+  }
+  emailChange = event => {
+    this.setState({ email: event.target.value });
+  }
+  passwordChange = event => {
+    this.setState({ password: event.target.value });
+  }
+
+  cadastroConfirmado = () => this.setState({ realizado: true })
+
+  cadastroNegado = (erro) => this.setState({ erro: true, mensagemErro: "Sistema já possui usuário." })
+
+  voltar = () => this.setState({ erro: false })
+
+  irParaLogin = () => {
+    this.resetar()
+    this.props.history.push('/page')
+  }
+
+  resetar = () => {
+    this.setState({
+      username: "",
+      email: "",
+      password: "",
+      realizado: false,
+      erro: false,
+      mensagemErro: "",
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const user = {
+      "username": this.state.username,
+      "email": this.state.email,
+      "password": this.state.password,
+    };
+
+    axios.post("http://localhost:3001/user/", user).then(result => {
+      this.cadastroConfirmado()
+    }, error => {
+      this.cadastroNegado(error)
+    })
+  }
+
+  render() {
+    return (
+      <div className='login-form'>
+        <style>{`
       body > div,
       body > div > div,
       body > div > div > div.login-form {
         height: 100%;
       }
     `}</style>
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='black' textAlign='center'>
-          Create your account
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
-            <Button color='black' fluid size='large' as={NavLink} to='/page'>
-              Cadastre
-            </Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </div>
-)
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+           
+            <div>
+                <Form onSubmit={this.handleSubmit} size="large">
+                  <Segment stacked>
+                  <Header as='h2' color='black' textAlign='center'>
+                    Create your account
+                  </Header>
+                  <Form.Input required fluid icon='user' iconPosition='left' placeholder='Digite seu nome de usuário' onChange={this.nomeChange}/>
+                  <Form.Input required fluid icon='user' iconPosition='left' placeholder='Digite seu email' onChange={this.emailChange} />
+                  <Form.Input required fluid icon='lock' iconPosition='left' placeholder='Digite sua senha' type='password' onChange={this.senhaChange}/>
+                    <Button type="submit" color="black" >Cadastrar</Button>
+                    </Segment>
+                </Form>
 
-export default Cadastro
+                <Confirm
+                    open={this.state.realizado}
+                    header='Cadastro realizado'
+                    content='Você já pode realizar seu login.'
+                    confirmButton="Certo, logar agora"
+                    onConfirm={this.irParaLogin}  />
+                <Confirm
+                    open={this.state.erro}
+                    header='Erro no cadastro'
+                    content={this.state.mensagemErro}
+                    confirmButton="Corrigir"
+                    onConfirm={this.voltar} />
+            </div>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+const CadastroComRouter = withRouter(Cadastro);
+
+export default CadastroComRouter
--- nsei/src/Components/login/Login.js
+++ nsei/src/Components/login/Login.js
@@ -1,46 +1,97 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Form, Grid, Header, Message, Segment, Confirm } from 'semantic-ui-react'
 import { NavLink } from "react-router-dom";
-import ButtonSignUp from "../botoes/botoesSignUp";
+import axios from 'axios'
+import { withRouter } from 'react-router';
 
 
-const Login = () => (
-  <div className='login-form'>
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='black' textAlign='center'>
-          Log-in to your account
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
-            <Button color='black' fluid size='large' as={NavLink} to='/page'>
-              Login
+class Login extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: "",
+      erro: false
+    }
+  }
+
+  salvarToken(response) {
+    var res = response.data;
+    localStorage.setItem('userID', res.usuarioId);
+    localStorage.setItem("token", res.token);
+  }
+
+  emailChange = event => {
+    this.setState({ email: event.target.value });
+  }
+  passwordChance = event => {
+    this.setState({ password: event.target.value });
+  }
+  loginConfirmado = () => {
+    this.props.history.push('/page')
+  }
+
+  loginNegado = (erro) => this.setState({ erro: true, mensagemErro: "E-mail ou senha incorretos." })
+  voltar = () => this.setState({ erro: false })
+
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const user = {
+      "email": this.state.email,
+      "password": this.state.password,
+    };
+    axios.post("http://localhost:3001/authentication/", user).then(result => {
+      this.salvarToken(result)
+      this.loginConfirmado()
+    }, error => {
+      this.loginNegado(error)
+    })
+  }
+
+  render() {
+    return (
+      <div className='login-form'>
+        <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.login-form {
+          height: 100%;
+        }
+      `}</style>
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+          <div>
+            <Form onSubmit={this.handleSubmit} size="large">
+              <Segment stacked>
+              <Header as='h2' color='black' textAlign='center'>
+                Entre com sua conta
+              </Header>
+              <Form.Input required fluid icon='user' iconPosition='left' placeholder='Digite seu email' onChange={this.emailChange} />
+              <Form.Input required fluid icon='lock' iconPosition='left' placeholder='Password' type='password' onChange={this.passwordChange}/>
+              <Button type="submit" color='black' fluid size='large'> Login </Button>
+              </Segment>
+            </Form>
+            <Confirm
+            open={this.state.erro}
+            header='Erro no login'
+            content={this.state.mensagemErro}
+            confirmButton="Corrigir"
+            onConfirm={this.voltar} />
+        </div>
+            <Message>
+              Novo por aqui?
+            <Button color='black' fluid size='large' as={NavLink} to='/cadastro'>
+                Cadastre-se
             </Button>
-          </Segment>
-        </Form>
-        <Message>
-          Novo por aqui?
-          <ButtonSignUp>
-          /</ButtonSignUp>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  </div>
-)
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
+  }
+}
 
-export default Login
+const LoginComRouter = withRouter(Login);
+export default LoginComRouter; 
